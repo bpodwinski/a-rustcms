fd2ba13c07abd0b6523d6375540a454b42e3c67c
Benoit Podwinski <me@benoitpodwinski.com>
2024-09-17 16:04:05 +0200

    Change publish component in accordion bootstrap

--- src/components/admin/publish_component.rs
+++ src/components/admin/publish_component.rs
@@ -13,65 +13,87 @@ pub fn PublishComponent(
     set_date_published: WriteSignal<Option<NaiveDateTime>>,
 ) -> impl IntoView {
     view! {
-        <div class="publish-component card mb-5">
+        <div class="publish-component accordion mb-5" id="publish-component">
+            <div class="accordion-item">
 
-            <div class="card-header">
-                <h3>Publish</h3>
-            </div>
-
-            <div class="card-body">
-
-                <div class="mb-3">
-                    <h4>Status</h4>
-
-                    {PostStatusEnum::iter()
-                        .map(|status_option| {
-                            let status_option_str = format!("{:?}", status_option);
-                            let status_option = Rc::new(status_option);
-                            let status_option_clone = Rc::clone(&status_option);
-                            let status_option_clone2 = Rc::clone(&status_option);
-                            view! {
-                                <div class="form-check">
-                                    <input
-                                        class="form-check-input"
-                                        type="radio"
-                                        name="post-status"
-                                        id=status_option_str.clone()
-                                        on:change=move |_| {
-                                            set_status.set((*status_option_clone).clone())
-                                        }
-
-                                        prop:checked=move || *status_option_clone2 == status.get()
-                                    />
-                                    <label class="form-check-label" for=status_option_str.clone()>
-                                        {status_option_str}
-                                    </label>
-                                </div>
-                            }
-                        })
-                        .collect::<Vec<_>>()}
+                <div class="accordion-header">
+                    <button
+                        class="accordion-button"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#collapsePublish"
+                        aria-expanded="true"
+                        aria-controls="collapsePublish"
+                    >
+                        <h3>Publish</h3>
+                    </button>
                 </div>
 
-                <div>
-                    <h4 for="post-date-published" class="form-label">
-                        Date Published
-                    </h4>
+                <div
+                    id="collapsePublish"
+                    class="accordion-collapse collapse show"
+                    data-bs-parent="#publish-component"
+                >
+                    <div class="accordion-body">
 
-                    <input
-                        type="datetime-local"
-                        on:input=move |ev| {
-                            let date = event_target_value(&ev);
-                            let parsed_date = NaiveDateTime::parse_from_str(&date, "%Y-%m-%dT%H:%M")
-                                .ok();
-                            set_date_published.set(parsed_date);
-                        }
+                        <div class="mb-3">
+                            <h4>Status</h4>
 
-                        class="form-control"
-                        id="post-date-published"
-                    />
+                            {PostStatusEnum::iter()
+                                .map(|status_option| {
+                                    let status_option_str = format!("{:?}", status_option);
+                                    let status_option = Rc::new(status_option);
+                                    let status_option_clone = Rc::clone(&status_option);
+                                    let status_option_clone2 = Rc::clone(&status_option);
+                                    view! {
+                                        <div class="form-check">
+                                            <input
+                                                class="form-check-input"
+                                                type="radio"
+                                                name="post-status"
+                                                id=status_option_str.clone()
+                                                on:change=move |_| {
+                                                    set_status.set((*status_option_clone).clone())
+                                                }
+
+                                                prop:checked=move || *status_option_clone2 == status.get()
+                                            />
+                                            <label
+                                                class="form-check-label"
+                                                for=status_option_str.clone()
+                                            >
+                                                {status_option_str}
+                                            </label>
+                                        </div>
+                                    }
+                                })
+                                .collect::<Vec<_>>()}
+                        </div>
+
+                        <div>
+                            <h4 for="post-date-published" class="form-label">
+                                Date Published
+                            </h4>
+
+                            <input
+                                type="datetime-local"
+                                on:input=move |ev| {
+                                    let date = event_target_value(&ev);
+                                    let parsed_date = NaiveDateTime::parse_from_str(
+                                            &date,
+                                            "%Y-%m-%dT%H:%M",
+                                        )
+                                        .ok();
+                                    set_date_published.set(parsed_date);
+                                }
+
+                                class="form-control"
+                                id="post-date-published"
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
-
         </div>
     }
 }
